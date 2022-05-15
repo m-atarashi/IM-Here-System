91f5401ec2d2d2fc0989233559474b8d9771e4ea
m-atarashi <b1018235@fun.ac.jp>
2022-05-15 13:29:55 +0900

    fix: redesign

--- src/components/Attendance.js
+++ src/components/Attendance.js
@@ -31,36 +31,49 @@ export default function Attendance(props) {
   return (
     <>
       <Head>
-        <title>Where are you? - {lab}</title>
-        <link rel='icon' href='../../img/sumi.jpeg'/>
+        <title>Attendance System - {lab}</title>
+        <link rel='icon' href=''/>
       </Head>
-
-      <h1 align='center'>{lab}</h1>
-      <table className='table table-bordered table-striped'>
-        <thead className='thead-dark'>
-        {/*────────── draw a headr row like MEMBER, HOME, OUT, FUN, ... ──────────*/}
-          <tr>
-            <th>MEMBER</th>
-            {locations.map(location => <th>{location}</th>)}
-          </tr>
-        </thead>
-        {/*────────── draw table cells of each member and each location ──────────*/}
-        {members.map(member => {
-          return (
+      <div className='header'>
+        <div className='table-title'>
+          <span>Attendance System</span>
+          <div className='underline'></div>
+        </div>
+      </div>
+      <div className='attendance-table-container'>
+        <table className='attendance-table'>
+          <thead>
+          {/*────────── draw a headr row like MEMBER, HOME, OUT, FUN, ... ──────────*/}
             <tr>
-              <th>{member}</th>
-              {locations.map(location => {
-                return (
-                  location === membersLocation[member] ?  // display member's icon at the current location, otherwise an empty space which emits a 'onMoved' event when touched or clicked
-                    <td><img className='member-avatar' src={portraits[member]}/></td> :
-                    <td><button className='attendance-table-button' type='button' onClick={() => {socket.emit('memberMoved', member, location)}}></button></td>
-                )
-              })}
+              <th>MEM.</th>
+              {locations.map(location => <th className='location'><span>{location}</span></th>)}
             </tr>
-          )
-        })}
-      </table>
-      <style jsx>{styles}</style>
+          </thead>
+          {/*────────── draw table cells of each member and each location ──────────*/}
+          {members.map(member => {
+            return (
+              <tr className='members-row'>
+                <th className='member-name'>
+                  <span>{member}</span>
+                </th>
+                {locations.map(location => {
+                  return (
+                    location === membersLocation[member] ?  // display member's icon at the current location, otherwise buttons which emits a 'onMoved' event when touched or clicked
+                      <td className='image-cell'>
+                        <img className='member-avatar' src={portraits[member]}/>
+                      </td> :
+                      <td className='button-cell'>
+                        <button className='attendance-table-button' type='button' onClick={() => {socket.emit('memberMoved', member, location)}}></button>
+                      </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </table>
+      </div>
+      <gura />
+      <style jsx>{styles}</style> 
     </>
   )
 }
