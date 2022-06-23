2bc53567a8862bef7af47a91cb97e02b676f206e
m-atarashi <m-atarashi@sumilab.org>
2022-06-24 03:25:21 +0900

    create Message and Title compeonets

---
+++ components/Title.js
@@ -0,0 +1,11 @@
+'use strict'
+import styles from '../css/table_view.module.css'
+
+export default function Message(props) {
+  return (
+    <div className={styles.table_title}>
+      <span>IM Here System</span>
+      <div className={styles.underline}></div>
+  </div>
+  )
+}
