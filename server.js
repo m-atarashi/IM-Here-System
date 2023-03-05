cbe51151be69d3d2b37a84c3b3ef194d5005d771
m-atarashi <m-atarashi@sumilab.org>
2023-03-06 01:12:32 +0900

    fix; replace yml by json, delete "sendConfig" emit and refactered

--- server.js
+++ server.js
@@ -1,75 +1,98 @@
-'use strict'
+"use strict";
 
-const http = require('http')
-const next = require('next')
-const Server = require('socket.io')
-const { IncomingWebhook } = require('@slack/webhook');
-const { notifyLocation, notifyInClassTurned } = require('./scripts/slackNotification')
-const StayTimeManager = require('./scripts/StayTimeManager')
+const http = require("http");
+const next = require("next");
+const Server = require("socket.io");
+const { IncomingWebhook } = require("@slack/webhook");
+const {
+  notifyLocation,
+  notifyInClassTurned,
+} = require("./scripts/slackNotification");
+const StayTimeManager = require("./scripts/StayTimeManager");
 
+const config = require("./public/config.json");
+const members = config.members.map((e) => Object.keys(e)[0]);
+const memberLocations = members.reduce((obj, member, _) => {
+  obj[member] = "HOME";
+  return obj;
+}, {});
+const membersInClass = members.reduce((obj, member, _) => {
+  obj[member] = false;
+  return obj;
+}, {});
+const stayTimeManagers = members.reduce((obj, member, _) => {
+  obj[member] = new StayTimeManager();
+  return obj;
+}, {});
 
-const config = require('js-yaml').load(require('fs').readFileSync('public/config.yml'))
-const members = Object.keys(config.members)
+const getLocationSlackDisplayName = (location) =>
+  config.locations.filter((e) => e[location])[0][location];
+const webhook =
+  config.slackWebhookURL === ""
+    ? undefined
+    : new IncomingWebhook(config.slackWebhookURL);
 
-const memberLocations = members.reduce((obj, member, _) => {obj[member] = 'HOME'; return obj}, {})
-const membersInClass = members.reduce((obj, member, _) => {obj[member] = false; return obj}, {})
-const stayTimeManagers = members.reduce((obj, member, _) => {obj[member] = new StayTimeManager(); return obj}, {})
-
-const getLocationSlackDisplayName = (location) => config.locations.filter(e => e[location])[0][location]
-const webhook = new IncomingWebhook(config.slackWebhookURL)
-
-const dev = process.env.NODE_ENV !== 'production'
-const nextApp = next({ dev })
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
 nextApp.prepare().then(
   () => {
-    const server = http.createServer(nextApp.getRequestHandler()).listen(3000)
-    const io = Server(server)
+    const server = http.createServer(nextApp.getRequestHandler()).listen(3000);
+    const io = Server(server);
 
-    io.on('connection', socket => {
+    io.on("connection", (socket) => {
       // send data to a new connected client
-      socket.emit('updateMemberLocations', memberLocations)
-      socket.emit('updateMembersInClass', membersInClass)
-      socket.emit('sendConfig', config)
-      
-      socket
-        .on('memberMoved', (member, location) => {
-          const prevLoc = memberLocations[member]
-          const stayTimeManager = stayTimeManagers[member]
-          
-          if (prevLoc === location) return
-          // come to school
-          if (prevLoc === 'HOME') stayTimeManager.init()
-          // go home
-          if (location === 'HOME') {
-            stayTimeManager.setStayMinute()
-            stayTimeManager.updateLastMovedTime()
-            // turn off class toggle
-            membersInClass[member] = false
+      socket.emit("updateMemberLocations", memberLocations);
+      socket.emit("updateMembersInClass", membersInClass);
 
-            io.emit('updateMembersInClass', membersInClass)
+      socket
+        .on("memberMoved", (member, location) => {
+          const prevLoc = memberLocations[member];
+          const stayTimeManager = stayTimeManagers[member];
+
+          if (prevLoc === location) return;
+          // come to school
+          if (prevLoc === "HOME") stayTimeManager.init();
+          // go home
+          if (location === "HOME") {
+            stayTimeManager.setStayMinute();
+            stayTimeManager.updateLastMovedTime();
+            // turn off class toggle
+            membersInClass[member] = false;
+            io.emit("updateMembersInClass", membersInClass);
           }
 
           // notify to Slack
-          notifyLocation(webhook, member, getLocationSlackDisplayName(prevLoc), getLocationSlackDisplayName(location), stayTimeManager)
+          notifyLocation(
+            webhook,
+            member,
+            getLocationSlackDisplayName(prevLoc),
+            getLocationSlackDisplayName(location),
+            stayTimeManager
+          );
           // update stay time
-          stayTimeManager.updateLastMovedTime()
+          stayTimeManager.updateLastMovedTime();
           // update a member's location
-          memberLocations[member] = location
+          memberLocations[member] = location;
 
-          io.emit('updateMemberLocations', memberLocations)
+          io.emit("updateMemberLocations", memberLocations);
         })
-        .on('classTurned', (member, location) => {
+        .on("classTurned", (member, location) => {
           // update whether the member is in class or not
-          membersInClass[member] = !membersInClass[member]
+          membersInClass[member] = !membersInClass[member];
           // notify to Slack
-          notifyInClassTurned(webhook, member, getLocationSlackDisplayName(location), membersInClass[member])
+          notifyInClassTurned(
+            webhook,
+            member,
+            getLocationSlackDisplayName(location),
+            membersInClass[member]
+          );
 
-          io.emit('updateMembersInClass', membersInClass)
-        })
-    })
+          io.emit("updateMembersInClass", membersInClass);
+        });
+    });
   },
-  err => {
-    console.error(err)
-    process.exit(1)
+  (err) => {
+    console.error(err);
+    process.exit(1);
   }
-)
+);
