3486063bc0bb573256150490aeb6bfd13a84f211
m-atarashi <m-atarashi@sumilab.org>
2023-03-06 01:11:21 +0900

    fix: add "key" properties to iterated elements and

--- components/TableMain.js
+++ components/TableMain.js
@@ -1,31 +1,36 @@
-'use strict'
-import styles from '../css/table_view.module.css'
-import Abator from './Abator'
-import ClassToggleButton from './ClassToggleButton'
-import LocationButton from './LocationButton'
+"use strict";
+import styles from "../css/table_view.module.css";
+import Abator from "./Abator";
+import ClassToggleButton from "./ClassToggleButton";
+import LocationButton from "./LocationButton";
 
 /*────────── draw table cells of each member and each location ──────────*/
 export default function TableMain(props) {
   return (
-  <>
-    {props.members.map(member => {
-      return (
-        <tr className={styles.members_row}>
-          <th className={styles.member_name}>
-            <span>{member}</span>
-          </th>
-          {props.locations.map(location => {
-            return (
-              location === props.membersLocation[member] ?
-                <Abator member={member} {...props}/>:
-              location === 'CLASS' ?
-                <ClassToggleButton member={member} {...props}/> :
-                <LocationButton member={member} location={location} {...props}/>
-            )
-          })}
-        </tr>
-      )
-    })}
-  </>
-  )
+    <>
+      {props.members.map((member, index) => {
+        return (
+          <tr className={styles.members_row} key={index}>
+            <th className={styles.member_name}>
+              <span>{member}</span>
+            </th>
+            {props.locations.map((location, jndex) => {
+              return location === props.membersLocation[member] ? (
+                <Abator src={props.portraits[member]} key={jndex} />
+              ) : location === "CLASS" ? (
+                <ClassToggleButton member={member} key={jndex} {...props} />
+              ) : (
+                <LocationButton
+                  member={member}
+                  location={location}
+                  key={jndex}
+                  {...props}
+                />
+              );
+            })}
+          </tr>
+        );
+      })}
+    </>
+  );
 }
