41c12717dd0cf48c8c947eb759347fc4c81dec25
m-atarashi <m-atarashi@sumilab.org>
2023-03-05 13:12:31 +0900

    fix: replace img to Image from next/image

--- components/Abator.js
+++ components/Abator.js
@@ -1,10 +1,18 @@
-'use strict'
-import styles from '../css/table_view.module.css'
+"use strict";
+import Image from "next/image";
+import styles from "../css/table_view.module.css";
 
 export default function Abator(props) {
   return (
     <td className={styles.image_cell}>
-      <img className={styles.member_avatar} src={props.portraits[props.member]}/>
-    </td> 
-  )
+      <div className={styles.member_avatar}>
+        <Image
+          className={styles.member_avatar}
+          src={props.portraits[props.member]}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </td>
+  );
 }
