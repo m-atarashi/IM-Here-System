05a634bdfb6577d4b093764fb61bd6726cd53304
m-atarashi <m-atarashi@sumilab.org>
2023-03-06 01:14:51 +0900

    fix: deleted unneccessary useState, deal with Json config

--- components/TableView.js
+++ components/TableView.js
@@ -1,49 +1,59 @@
-'use strict'
+"use strict";
 
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import io from 'socket.io-client'
-import styles from '../css/table_view.module.css'
-import Header from './Header'
-import Table from './Table'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import styles from "../css/table_view.module.css";
+import Header from "./Header";
+import Table from "./Table";
 
 export default function TableView(props) {
-  const [socket, setSocket] = useState()
-  const [membersLocation, setmMembersLocation] = useState([])
-  const [membersInClass, setMembersInClass] = useState({})
-  const [lab, setLab] = useState()
-  const [members, setMembers] = useState([])
-  const [locations, setLocations] = useState([])
-  const [portraits, setPortraits] = useState({})
-  const [message, setMessage] = useState()
-  const [isScroll, setisScroll] = useState()
-  
+  const tmp = Object.assign(...props.members);
+  Object.keys(tmp).forEach((key) => (tmp[key] = "HOME"));
+  const [membersLocation, setmMembersLocation] = useState(tmp);
+  Object.keys(tmp).forEach((key) => (tmp[key] = false));
+  const [membersInClass, setMembersInClass] = useState(tmp);
+
+  const [socket, setSocket] = useState();
   useEffect(() => {
-    const socket = io()
+    const socket = io();
     socket
-      .on('updateMemberLocations', membersLocation => setmMembersLocation(membersLocation)) // get current membersLocation status
-      .on('updateMembersInClass', membersInClass => setMembersInClass(membersInClass))
-      .on('sendConfig', config => { // get config and set lab name, members, locations, portraits
-        setLab(config.lab)
-        setMembers(Object.keys(config.members))
-        setLocations(config.locations.map(e => Object.keys(e)[0]))
-        setPortraits(config.members)
-        setMessage(config.message)
-        setisScroll(config.isScroll)
-      })
-    setSocket(socket)
-    return () => socket.close() // close this socket when component cleanuped
-  }, [])
-  
+      .on("updateMemberLocations", (membersLocation) =>
+        setmMembersLocation(membersLocation)
+      ) // get current membersLocation status
+      .on("updateMembersInClass", (membersInClass) =>
+        setMembersInClass(membersInClass)
+      )
+      .on("sendConfig", );
+    setSocket(socket);
+    return () => socket.close(); // close this socket when component cleanuped
+  }, []);
+
   return (
     <>
       <Head>
-        <title>IM Here System - {lab}</title>
+        <title>{`IM Here System - ${props.lab}`}</title>
       </Head>
-      <Header message={message} isScroll={isScroll}/>
+      <Header message={props.message} isScroll={props.isScroll} />
       <div className={styles.attendance_table_container}>
-        <Table socket={socket} members={members} locations={locations} portraits={portraits} membersLocation={membersLocation} membersInClass={membersInClass} message={message}/>
+        <Table
+          socket={socket}
+          members={props.members.map((e) => Object.keys(e)[0])}
+          portraits={Object.assign(...props.members)}
+          locations={props.locations.map((e) => Object.keys(e)[0])}
+          membersLocation={membersLocation}
+          membersInClass={membersInClass}
+          message={props.message}
+        />
       </div>
     </>
-  )
+  );
 }
+
+import config from "../public/config.json";
+export async function getStaticProps() {
+  return {
+    props: {...config},
+  }
+}
+
